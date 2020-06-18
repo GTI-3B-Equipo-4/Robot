bc6ec9756d97f72cd2af41c4c358f40e1bf1b793
emilioesteve <36475580+emilioesteve@users.noreply.github.com>
2020-06-18 03:19:52 +0200

    Comentarios PyDoc

--- dream_team_package/battery_state/src/Bateria.py
+++ dream_team_package/battery_state/src/Bateria.py
@@ -1,21 +1,22 @@
 #! usr/bin/env python
 #-*- coding:utf-8 -*-
 
-# Autor: Emilio Esteve Peiró
-# Fecha: 10 03 2020
-# Descripción: Clase para obtener el porcentaje de batería que le queda al robot
-
+"""
+:FILE_NAME: Bateria.py
+:AUTHOR: Emilio Esteve Peiro
+:DATE: 10/03/2020
+:DESCRIPTION: Clase para obtener el porcentaje de bateria que le queda al robot
+"""
 import rospy
 from sensor_msgs.msg import BatteryState
 
 
 class LectorDeBateria(object):
-
-    # ------------------------------------------------------------------------------
-    # topic_name: Texto -->
-    # constructor() -->
-    # _topic_name:Texto, _sub:rospy.Suscriber(), _batterydata:BatteryState()
-    # ------------------------------------------------------------------------------
+    """
+    :function_name: constructor()
+    :input: topic_name: Texto
+    :output: _topic_name: Texto, _sub: rospy.Suscriber(), _batterydata: BatteryState()
+    """
     def __init__(self, topic_name='/battery_state'):
         self._topic_name = topic_name
         self._sub = rospy.Subscriber(self._topic_name, BatteryState, self.topic_callback)
@@ -25,9 +26,10 @@ class LectorDeBateria(object):
         self._batterydata = msg
         rospy.logdebug(self._batterydata)
 
-    # ------------------------------------------------------------------------------
-    # getPorcentajeDeBateriaRestante()
-    # --> R
-    # ------------------------------------------------------------------------------
+    """
+    :function_name: getPorcentajeDeBateriaRestante()
+    :input:
+    :output: R
+    """
     def getPorcentajeDeBateriaRestante(self):
         return self._batterydata.percentage
